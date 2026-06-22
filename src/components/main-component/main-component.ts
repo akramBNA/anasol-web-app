488356e054ab09c48425a69bcc909a0ccdf31edc
Akram <akrambenaoun1993@gmail.com>
2026-06-22 17:59:58 +0100

    create a main-component

--- src/components/main-component/main-component.ts
+++ src/components/main-component/main-component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
