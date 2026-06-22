7feb91ee4fae202905c4fffe38305abd244f94bb
Akram <akrambenaoun1993@gmail.com>
2026-06-22 19:47:32 +0100

    Add who-are-we component into main

--- src/components/main-component/main-component.ts
+++ src/components/main-component/main-component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Navbar } from '../navbar/navbar';
+import { WhoAreWe } from '../who-are-we/who-are-we';
 
 @Component({
   selector: 'app-main-component',
-  imports: [CommonModule, Navbar],
+  imports: [CommonModule, Navbar, WhoAreWe],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
