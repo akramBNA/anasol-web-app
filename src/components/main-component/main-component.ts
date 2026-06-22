650992d3c649c43f7b91aaebb788a4206c19d7a7
Akram <akrambenaoun1993@gmail.com>
2026-06-22 21:13:55 +0100

    ADD activities into main component

--- src/components/main-component/main-component.ts
+++ src/components/main-component/main-component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Navbar } from '../navbar/navbar';
 import { WhoAreWe } from '../who-are-we/who-are-we';
+import { Activities } from '../activities/activities';
 
 @Component({
   selector: 'app-main-component',
-  imports: [CommonModule, Navbar, WhoAreWe],
+  imports: [CommonModule, Navbar, WhoAreWe, Activities],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
