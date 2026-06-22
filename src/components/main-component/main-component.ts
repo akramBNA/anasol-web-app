49c21b59eb14aac8c1b61f116f70432e231b0cc1
Akram <akrambenaoun1993@gmail.com>
2026-06-22 18:09:05 +0100

    create a new navbar component

--- src/components/main-component/main-component.ts
+++ src/components/main-component/main-component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-main-component',
-  imports: [CommonModule],
+  imports: [CommonModule, Navbar],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
