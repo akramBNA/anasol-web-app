7c458e1594e6c96944f46267eb66101d9880e4e9
Akram <akrambenaoun1993@gmail.com>
2026-06-23 16:19:46 +0100

    ADD contact component into main-component

--- src/components/main-component/main-component.ts
+++ src/components/main-component/main-component.ts
@@ -5,10 +5,11 @@ import { WhoAreWe } from '../who-are-we/who-are-we';
 import { Activities } from '../activities/activities';
 import { Projects } from '../projects/projects';
 import { Testimonials } from '../testimonials/testimonials';
+import { Contact } from '../contact/contact';
 
 @Component({
   selector: 'app-main-component',
-  imports: [CommonModule, Navbar, WhoAreWe, Activities, Projects, Testimonials],
+  imports: [CommonModule, Navbar, WhoAreWe, Activities, Projects, Testimonials, Contact],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
