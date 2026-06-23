26c57bd58ed5335d8b9fb8a200ea06aafcc37919
Akram <akrambenaoun1993@gmail.com>
2026-06-23 15:27:20 +0100

    ADD projects component to main-components

--- src/components/main-component/main-component.ts
+++ src/components/main-component/main-component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Navbar } from '../navbar/navbar';
 import { WhoAreWe } from '../who-are-we/who-are-we';
 import { Activities } from '../activities/activities';
+import { Projects } from '../projects/projects';
 
 @Component({
   selector: 'app-main-component',
-  imports: [CommonModule, Navbar, WhoAreWe, Activities],
+  imports: [CommonModule, Navbar, WhoAreWe, Activities, Projects],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
