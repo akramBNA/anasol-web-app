1b47d204adab37686e87b852476823d596a106a2
Akram <akrambenaoun1993@gmail.com>
2026-06-23 17:26:21 +0100

    ADD footer.ts file

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.css',
+  styleUrls: ['./footer.css']
 })
 export class Footer {
+
+  currentYear: number = new Date().getFullYear();
+
+  scrollToTop(): void {
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+
+  }
 
 }
