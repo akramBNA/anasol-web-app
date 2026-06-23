52a689811e7b41a7f4b55323d29d62bc7b777094
Akram <akrambenaoun1993@gmail.com>
2026-06-23 16:23:59 +0100

    ADD contact icons instead of emojies

--- src/components/contact/contact.ts
+++ src/components/contact/contact.ts
@@ -10,6 +10,7 @@ import {
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact',
@@ -19,7 +20,8 @@ import { MatButtonModule } from '@angular/material/button';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './contact.html',
   styleUrls: ['./contact.css']
