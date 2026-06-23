48c7735eec62102db20e8393c7c5e05663666c3d
Akram <akrambenaoun1993@gmail.com>
2026-06-23 16:18:55 +0100

    ADD contact form and map

--- src/components/contact/contact.ts
+++ src/components/contact/contact.ts
@@ -1,11 +1,66 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './contact.html',
-  styleUrl: './contact.css',
+  styleUrls: ['./contact.css']
 })
 export class Contact {
 
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.contactForm = this.fb.group({
+
+      firstName: ['', Validators.required],
+
+      lastName: ['', Validators.required],
+
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+
+      phone: ['', Validators.required],
+
+      company: [''],
+
+      message: ['', [
+        Validators.required,
+        Validators.minLength(10)
+      ]]
+    });
+  }
+
+  submitForm(): void {
+
+    if (this.contactForm.valid) {
+
+      console.log(this.contactForm.value);
+
+      alert('Message envoyé avec succès.');
+
+      this.contactForm.reset();
+    }
+  }
 }
