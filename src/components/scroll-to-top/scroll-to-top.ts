85baec9c743ace1256192538af88ed5e26551d98
Akram <akrambenaoun1993@gmail.com>
2026-06-23 17:30:19 +0100

    CREATE a scroll-to-top component

---
+++ src/components/scroll-to-top/scroll-to-top.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  imports: [],
+  templateUrl: './scroll-to-top.html',
+  styleUrl: './scroll-to-top.css',
+})
+export class ScrollToTop {
+
+}
