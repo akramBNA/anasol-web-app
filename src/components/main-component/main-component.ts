6d865061a5f283bd2736cbadd724c865dcf660bd
Akram <akrambenaoun1993@gmail.com>
2026-06-23 17:33:47 +0100

    ADD a scroll-to-top into main-component

--- src/components/main-component/main-component.ts
+++ src/components/main-component/main-component.ts
@@ -7,10 +7,11 @@ import { Projects } from '../projects/projects';
 import { Testimonials } from '../testimonials/testimonials';
 import { Contact } from '../contact/contact';
 import { Footer } from '../footer/footer';
+import { ScrollToTop } from '../scroll-to-top/scroll-to-top';
 
 @Component({
   selector: 'app-main-component',
-  imports: [CommonModule, Navbar, WhoAreWe, Activities, Projects, Testimonials, Contact, Footer],
+  imports: [CommonModule, Navbar, WhoAreWe, Activities, Projects, Testimonials, Contact, Footer, ScrollToTop],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
