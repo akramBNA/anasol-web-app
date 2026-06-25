016eb73ccb3ff9264e452e18b21917c7cee241eb
Akram <akrambenaoun1993@gmail.com>
2026-06-25 21:51:44 +0100

    FIX router link for default-redirect

--- src/components/default-redirect/default-redirect.ts
+++ src/components/default-redirect/default-redirect.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-default-redirect',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './default-redirect.html',
   styleUrl: './default-redirect.css',
 })
-export class DefaultRedirect {
-
-}
+export class DefaultRedirect {}
