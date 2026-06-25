8c5a2f4750e8e3e871df6aa66d6df59bd8676d1a
Akram <akrambenaoun1993@gmail.com>
2026-06-25 21:44:59 +0100

    CREATE a defualt-redirect route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from '../components/main-component/main-component';
+import { DefaultRedirect } from '../components/default-redirect/default-redirect';
 
 export const routes: Routes = [
-    { path: '', component: MainComponent },
+  { path: '', component: MainComponent },
+  { path: '**', component: DefaultRedirect },
 ];
